c35e67837aed8b97b1430495ec2d24bca152350e
jogasozinho <gabrielauzierdev@gmail.com>
2022-09-06 12:08:39 -0300

    primeiro commit

---
+++ .vscode/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+float a, b, c;
+printf("digite um numero A\n");
+scanf("%f", &a);
+
+printf("agora digite um numero B\n");
+scanf("%f", &b);
+
+c = pow(a+b, 2);
+
+printf("A + B elevado ao quadrado e igual a: %f", c);
+}
